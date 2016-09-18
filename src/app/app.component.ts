5dfe7df310ee85ef9c4b618e2d6f8a1537e3dd84
Andrew Paradi <andrewparadi@gmail.com>
2016-09-18 04:16:46 -0400

    htn2016

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,96 @@
 import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable, AuthProviders, AuthMethods } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+// @Pipe({
+//   name: 'reverse'
+// })
+// export class ReversePipe {
+//   transform(value) {
+//     return value.slice().reverse();
+//   }
+// }
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'Resume Scoreboard';
+  userAuth: any;
+  jobTitle="";
+  jobDescr="";
+  resumeText="";
+  views:[
+    {
+      name: "My Dashboard",
+      description: "See your current resumes",
+      icon: "folder shared"
+    },
+    {
+      name: "My Dashboard",
+      description: "See current resumes",
+      icon: "folder shared"
+    }
+  ];
+  resumes: FirebaseListObservable<any[]>;
+  resumesDisplay: FirebaseListObservable<any[]>;
+  constructor(private af: AngularFire) {
+    this.af.auth.subscribe(user=>{
+      this.userAuth=user;
+      console.log(user);
+      this.resumes = af.database.list('/users/'+user.uid); //all resumes
+      this.resumesDisplay = af.database.list('/users/'+user.uid, {
+        query: {
+          limitToLast: 10,
+          orderByKey: true
+        }
+      }); //for displaying cards, only shows last 4
+    });
+  }
+  //ng on init, ng on destroy. implement from copnent class, call back to angular 2...
+  more() {
+
+  }
+  // save(auth: string, name: string) {
+  //   this.users.update({ auth: {"name": name} });
+  // }
+  update() {
+    if (
+      this.jobTitle != "" && this.jobDescr != "" && this.resumeText != ""
+    ) this.resumes.push(
+      { "name": this.userAuth.auth.displayName,
+        "authId": this.userAuth.auth.uid,
+        "timeStamp": Date.now(),
+        "jobTitle" : this.jobTitle,
+        "jobDescr" : this.jobDescr,
+        "resumeText" : this.resumeText,
+        "score" : this.jobDescr.length/200
+      });
+
+
+
+    console.log(
+      { "name": this.userAuth.auth.displayName,
+        "authId": this.userAuth.auth.uid,
+        "jobTitle" : this.jobTitle,
+        "jobDescr" : this.jobDescr,
+        "resumeText" : this.resumeText,
+        "score" : this.jobDescr.length/200 });
+    // console.log( { this.userAuth: { "resumes" : { "time" : this.time, "user": this.userAuth.displayName, "jobTitle" : this.jobTitle, "jobDescr": this.jobDescr, "resumeText" : this.resumeText}}});
+    // this.users.update( {this.user.auth: { resumes: { time: this.time, user: this.user.displayName, jobTitle:this.jobTitle, jobDescr: this.jobDescr, resumeText:this.resumeText}}});
+  }
+  delete() {
+    this.resumesDisplay.remove();
+  }
+  onGoogleAuth() {
+    this.af.auth.login({
+      provider: AuthProviders.Google,
+      method: AuthMethods.Popup,
+    })
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,45 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { MdButtonModule } from '@angular2-material/button';
+import { MdButtonToggleModule } from '@angular2-material/button-toggle';
+import { MdCardModule } from '@angular2-material/card';
+import { MdCheckboxModule } from '@angular2-material/checkbox';
+import { MdIconModule } from '@angular2-material/icon';
+import { MdInputModule } from '@angular2-material/input';
+import { MdSidenavModule } from '@angular2-material/sidenav';
+import { MdToolbarModule } from '@angular2-material/toolbar';
+//npm install --save @angular2-material/core @angular2-material/button @angular2-material/card @angular2-material/toolbar @angular2-material/checkbox @angular2-material/icon @angular2-material/input
+
+import { AngularFireModule } from 'angularfire2';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyCbKwfqOSZyc_TKNZ4YhChsztjfn42HsN4",
+  authDomain: "resume-scoreboard.firebaseapp.com",
+  databaseURL: "https://resume-scoreboard.firebaseio.com",
+  storageBucket: "resume-scoreboard.appspot.com"
+};
+
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     FormsModule,
-    HttpModule
+    HttpModule,
+    MdButtonModule,
+    MdButtonToggleModule,
+    MdCardModule,
+    MdCheckboxModule,
+    MdIconModule,
+    MdInputModule,
+    MdSidenavModule,
+    MdToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
